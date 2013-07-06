10b38de1da190021344436449b43b7c196f44b48
Ubuntu <ubuntu@ip-10-39-113-104.ec2.internal>
2013-07-06 19:41:30 +0000

    fs reading file

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var message = fs.readFileSync(index.html,'utf8');
+var message = fs.readFileSync('index.html','utf8');
 console.log(message);
 
 app.get('/', function(request, response) {
